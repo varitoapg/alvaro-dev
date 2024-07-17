09d927e1d9b1b8031095a1f642e44880212ab8ba
Alvaro Parada <alvaro.parada92@gmail.com>
2024-07-17 18:02:47 +0200

    Implement StringFormater in AboutMe section

---
+++ src/data/aboutMe.ts
@@ -0,0 +1,5 @@
+export const aboutMe: string[] = [
+  "Comencé mi carrera con un título en ingeniería y luego me trasladé al desarrollo web, impulsado por la pasión de crear código limpio, siguiendo las mejores prácticas para la escalabilidad y el mantenimiento.",
+  "Testing es un aspecto crucial de mi trabajo: me enorgullezco de asegurarme de que cada proyecto cumpla con altos estándares de fiabilidad y satisfacción del usuario. Este enfoque meticuloso garantiza que mis desarrollos sean robustos y efectivos.",
+  "Además de habilidades técnicas, aporto fuertes habilidades blandas: capacidad de aprendizaje, pensamiento analítico y flexibilidad. Estas cualidades me permiten adaptarme rápidamente a nuevos desafíos y colaborar eficazmente dentro de equipos para ofrecer soluciones digitales impactantes."
+];
