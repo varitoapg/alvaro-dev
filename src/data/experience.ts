0efc96686c54c5ed3f81d4f32c36db2771e53f51
Alvaro Parada <alvaro.parada92@gmail.com>
2024-07-17 18:02:22 +0200

    Improve experience description and implement StringFormater

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -5,9 +5,10 @@ export const experiences: Experience[] = [
   {
     company: "Estoko Logistics",
     description: [
-      "- Desarrollo del ERP de la empresa, que utiliza nuevas tecnologías como PostgreSQL, Next.js, Apollo GraphQL y Prisma, enfocándome en el desarrollo frontend y brindando soporte backend ocasional para garantizar un enfoque total.",
-      "- Mantenimiento de varias aplicaciones, priorizando la optimización del frontend y las mejoras de la interfaz de usuario para una experiencia de usuario perfecta.",
-      "- Colaboración estrecha con el tech lead y el project manager para garantizar una coordinación eficiente del equipo en una metodología Agile, manteniendo un ambiente de trabajo altamente productivo usando Jira."
+      "- Mantenimiento del ERP de la empresa e implementación de nuevas features con NextJS, React, TypeScript y una base de datos MySQL.",
+      "- Diseño y desarrollo de un nuevo microservicio del ERP, con una API GraphQL, capaz de manejar la información de más de 1000 almacenes y reduciendo la captación de clientes de casi 1 semana a menos de 1 día.",
+      "- Optimización de endpoints y queries SQL, llegando a reducir de 20s a menos de 1s el tiempo de respuesta.",
+      "- Mejora del entorno Agile estandarizando las definiciones de las tareas en Jira y reduciendo el tiempo de las dailies de más de 30 min a menos de 10."
     ],
     endDate: "Actual",
     startDate: "Abr. 2021",
@@ -39,9 +40,9 @@ export const experiences: Experience[] = [
   {
     company: "Freelance",
     description: [
-      "- Desarrollé SPAs usando un stack MERN, con Redux para la gestión del estado.",
-      "- Trabajé con un equipo de programadores full stack y un tech lead utilizando metodología Agile para crear una aplicaciones con buenas prácticas.",
-      "- Creé test unitarios y de integración."
+      "- Desarrollé SPAs usando un stack MERN (Mongo, Express, React, Node), con Redux para la gestión del estado.",
+      "- Implementé de test unitarios con Jest, subiendo el coverage hasta estar por encima del 80%.",
+      "- Mejoré la UX/UI de las aplicaciones, aumentando los valores de LightHouse a más de 90."
     ],
     endDate: "Dic. 2021",
     startDate: "Sept. 2021",
