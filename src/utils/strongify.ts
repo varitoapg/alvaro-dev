b4d361afdf6b7e30aed3c287fac51b59e86f0be4
Alvaro Parada <alvaro.parada92@gmail.com>
2024-07-17 14:33:35 +0200

    Create a function to prevent XSS while set html

--- src/utils/strongify.ts
+++ src/utils/strongify.ts
@@ -4,7 +4,7 @@ export const strongify = (text: string): string => {
   const escapedDictionary = dictionary.map((word) =>
     word.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")
   );
-  const regex = new RegExp(`\\b(${escapedDictionary.join("|")})\\b`, "gi"); // Create regex to match dictionary words
+  const regex = new RegExp(`\\b(${escapedDictionary.join("|")})\\b`, "gi");
 
   return text.replace(regex, '<strong class="text-primary">$1</strong>');
 };
